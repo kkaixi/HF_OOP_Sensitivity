c8dfb38358b247d8dee63229fe8a71c4d1303bda
kkaixi <35850858+kkaixi@users.noreply.github.com>
2019-05-24 17:04:33 -0400

    2019-05-24

---
+++ HF_OOP_Sensitivity_plot.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 12:23:32 2019
+
+@author: tangk
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly_express as px
+from PMG.COM.plotfuns import *
+from PMG.COM.arrange import arrange_by_group
+from initialize import dataset
+
+dataset.get_data(['features', 'timeseries', 'stats'])
+
+#%% get statistically significant differences in features
+sig_ch = condense_df(pd.concat(dataset.stats.values(), axis=1))
+sig_ch.columns = list(dataset.stats.keys())
+
+sig_ch_all = condense_df(sig_ch[['std_vs_oop_wilcox','std_vs_oop_t']])
+condense_df(sig_ch.drop(sig_ch_all.index)[['std2_vs_oop_wilcox']])
+#%% plot confidence bands
+#plot_channels = ['13HEAD0000HFACYA',
+#                 '13CHST0000HFACYC',
+#                 '13PELV0000HFACYA',
+#                 '13NECKUP00HFFOXA',
+#                 '13NECKUP00HFFOYA',
+#                 '13CHST0000HFDSXB']
+plot_channels = dataset.channels
+for ch in plot_channels:
+    fig, ax = plt.subplots()
+    x1 = arrange_by_group(dataset.table.query('HF_POS<3'), dataset.timeseries[ch], 'HF_POS')
+    x2 = arrange_by_group(dataset.table.query('HF_POS==3'), dataset.timeseries[ch], 'MODEL')
+    ax, lines = plot_bands(ax, dataset.t, x1, legend=False)
+    ax = plot_overlay(ax, dataset.t, x2, line_specs={k: {'linewidth': 0.5, 'color': 'b'} for k in x2.keys()})
+    lines.extend(ax.lines[-len(x2):])
+    ax = set_labels(ax, {'title': ch})
+    ax.legend(lines, ['STD POS 1','STD POS 2'] + list(x2.keys()), bbox_to_anchor=(1,1))
+
+#%% plot peak values
+plot_channels = ['Max_13HEAD0000HFACYA',
+                 'Max_13CHST0000HFACYC',
+                 'Max_13PELV0000HFACYA',
+                 'Max_13NECKUP00HFFOYA',
+                 'Min_13HEAD0000HFACYA',
+                 'Min_13CHST0000HFACYC',
+                 'Min_13PELV0000HFACYA',
+                 'Min_13NECKUP00HFFOYA',
+                 'Min_13CHST0000HFDSXB',
+                 'Min_13NECKUP00HFFOXA',
+                 'Min_13FEMRLE00HFFOZB',
+                 'Min_13FEMRRI00HFFOZB',
+                 'Min_13ILACLE00HFFOXA']
+for ch in plot_channels:
+    fig, ax = plt.subplots()
+    ax = sns.barplot(x='HF_POS', y=ch, data=pd.concat((dataset.features[ch].abs(), dataset.table['HF_POS']), axis=1))
+    ax = set_labels(ax, {'title': ch})
+
+
+#%% plot peak values as a function of installation stuff
+chx_list = ['SEATBACK_ANGLE',
+            'SLOUCH',
+            'PELVIS_ANGLE',
+            'HEAD_CG_Y-SEAT_Y']
+chy_list = ['Min_13HEAD0000HFACXA',
+            'Min_13NECKUP00HFFOXA',
+            'Max_13HEAD0000HFACYA',
+            'Max_13ILACLE00HFFOXA',
+            'Min_13FEMRLE00HFFOZB']
+
+subset = dataset.table.query('YEAR==2019')
+subset['SLOUCH'] = subset['SLOUCH'].replace(np.nan, 0)
+dataset.features['SLOUCH'] = dataset.features['SLOUCH'].replace(np.nan, 0)
+for chx in chx_list:
+    for chy in chy_list:
+        x = arrange_by_group(subset, dataset.features[chx], 'SLOUCH')
+        y = arrange_by_group(subset, dataset.features[chy], 'SLOUCH')
+        
+        fig, ax = plt.subplots()
+        ax = plot_scatter(ax, x, y)
+        ax = set_labels(ax, {'xlabel': chx, 'ylabel': chy})
+        
+        plt.show()
+        plt.close(fig)
+        
+#%% plot overlays (paired)
+plot_channels = dataset.channels
+
+grouped = dataset.table.groupby('HF_POS')
+pairs = np.intersect1d(grouped.get_group('OOP')['MODEL'],grouped.get_group('STD')['MODEL'])
+for ch in plot_channels:
+    for pair in pairs:
+        subset = dataset.table.query('MODEL==\'' + pair + '\'')
+        x = arrange_by_group(subset, dataset.timeseries[ch], 'HF_POS')
+        fig, ax = plt.subplots()
+        ax = plot_overlay(ax, dataset.t, x)
+        set_labels(ax, {'title': (pair,ch), 'legend': {}})
+        plt.show()
+        plt.close(fig)
+    
---
+++ initialize.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 12:51:24 2019
+
+@author: tangk
+"""
+
+from PMG.read_data import PMGDataset
+from PMG.COM.get_props import get_peaks
+import json
+import pandas as pd
+
+
+directory = 'P:\\Data Analysis\\Projects\\HF OOP Sensitivity\\'
+cutoff = range(100,1600)
+channels = ['13HEAD0000HFACXA','13HEAD0000HFACYA','13HEAD0000HFACZA','13HEAD0000HFACRA',
+            '13HEAD0000HFAVXD','13HEAD0000HFAVYD','13HEAD0000HFAVZD',
+            '13NECKUP00HFFOXA','13NECKUP00HFFOYA','13NECKUP00HFFOZA','13NECKUP00HFFORA',
+            '13NECKUP00HFMOXB','13NECKUP00HFMOYB','13NECKUP00HFMOZB','13NECKUP00HFMORB',
+            '13CLAVLE00HFFOXA','13CLAVLE00HFFOYA','13CLAVLE00HFFOZA','13CLAVLE00HFFORA',
+            '13CLAVRI00HFFOXA','13CLAVRI00HFFOYA','13CLAVRI00HFFOZA','13CLAVRI00HFFORA',
+            '13CHST0000HFACXC','13CHST0000HFACYC','13CHST0000HFACZC','13CHST0000HFACRC',
+            '13CHST0000HFDSXB',
+            '13LUSP0000HFFOXA','13LUSP0000HFFOYA','13LUSP0000HFFOZA','13LUSP0000HFFORA',
+            '13LUSP0000HFMOXA','13LUSP0000HFMOYA',
+            '13ILACLE00HFFOXA',
+            '13ILACLE00HFMOYA',
+            '13ILACRI00HFFOXA',
+            '13ILACRI00HFMOYA',
+            '13PELV0000HFACXA','13PELV0000HFACYA','13PELV0000HFACZA','13PELV0000HFACRA',
+            '13FEMRLE00HFFOZB',
+            '13FEMRRI00HFFOZB',
+            '13SEBE0000B3FO0D',
+            '13SEBE0000B6FO0D',
+            '10SIMELE00INACXD',
+            '10SIMERI00INACXD',
+            '10CVEHCG0000ACXD','10CVEHCG0000ACYD','10CVEHCG0000ACZD','10CVEHCG0000ACRD']
+
+table_filters = {'query': 'SPEED==48 and HF_POS>=1 and HF_POS<=3',
+                 'drop': ['TC18-209']}
+preprocessing = None
+
+dataset = PMGDataset(directory, channels=channels, cutoff=cutoff, verbose=False)
+dataset.table_filters = table_filters
+dataset.preprocessing = preprocessing
+
+if __name__=='__main__':
+    dataset.get_data(['timeseries'])
+    table = dataset.table
+    features = get_peaks(dataset.timeseries)
+    
+    faro_data = ['HF_POS','SEAT_Y','SEAT_TRACK','SEATBACK_ANGLE','PELVIS_ANGLE','HEAD_CG_Y']
+    features[faro_data] = dataset.table[faro_data]
+    features['HEAD_CG_Y-SEAT_Y'] = features['HEAD_CG_Y'] - features['SEAT_Y']
+    
+    features.to_csv(directory + 'features.csv')
+    
+    # json file specifying statistical tests to be done
+    to_JSON = {'project_name': '',
+               'directory'   : directory,
+               'cat'     : {'STD_1': dataset.table.query('HF_POS==1').index.tolist(),
+                            'STD_2': dataset.table.query('HF_POS==2').index.tolist(),
+                            'OOP': dataset.table.query('HF_POS==3').index.tolist(),
+                            'STD_ALL': dataset.table.query('HF_POS<3').index.tolist(),
+                            'ALL': dataset.table.index.tolist()},
+               'test'    : [{'name': 'std_vs_oop_wilcox',
+                             'test1': 'STD_ALL',
+                             'test2': 'OOP',
+                             'testname': 'wilcox.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'std_vs_oop_t',
+                             'test1': 'STD_ALL',
+                             'test2': 'OOP',
+                             'testname': 't.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'std1_vs_oop_wilcox',
+                             'test1': 'STD_1',
+                             'test2': 'OOP',
+                             'testname': 'wilcox.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'std1_vs_oop_t',
+                             'test1': 'STD_1',
+                             'test2': 'OOP',
+                             'testname': 't.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'std2_vs_oop_wilcox',
+                             'test1': 'STD_2',
+                             'test2': 'OOP',
+                             'testname': 'wilcox.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'std2_vs_oop_t',
+                             'test1': 'STD_2',
+                             'test2': 'OOP',
+                             'testname': 't.test',
+                             'data': 'features',
+                             'args': 'paired=FALSE, exact=FALSE, correct=TRUE, conf.level=0.95'},
+                            {'name': 'hf_pos_aov',
+                             'test1': 'ALL',
+                             'variables': ['HF_POS'],
+                             'formula': 'HF_POS',
+                             'testname': 'aov',
+                             'data': 'features',
+                             'model_args': None,
+                             'test_args': None}],
+                'test2'  : None}    
+    
+    for test in to_JSON['test']:
+        test['test1'] = to_JSON['cat'][test['test1']]
+        if 'test2' in test:
+            test['test2'] = to_JSON['cat'][test['test2']]
+    with open(directory+'params.json','w') as json_file:
+        json.dump(to_JSON,json_file)
+    
+
