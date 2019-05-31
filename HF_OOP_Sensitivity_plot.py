a6aa3190cb1dcbbbddd9914fd9941e7d1ef35ffc
kkaixi <35850858+kkaixi@users.noreply.github.com>
2019-05-31 13:04:32 -0400

    2019-05-31

--- HF_OOP_Sensitivity_plot.py
+++ HF_OOP_Sensitivity_plot.py
@@ -14,14 +14,15 @@ from PMG.COM.plotfuns import *
 from PMG.COM.arrange import arrange_by_group
 from initialize import dataset
 
-dataset.get_data(['features', 'timeseries', 'stats'])
+dataset.get_data(['features', 'stats'])
+dataset.get_data(['timeseries'])
 
 #%% get statistically significant differences in features
 sig_ch = condense_df(pd.concat(dataset.stats.values(), axis=1))
 sig_ch.columns = list(dataset.stats.keys())
 
 sig_ch_all = condense_df(sig_ch[['std_vs_oop_wilcox','std_vs_oop_t']])
-condense_df(sig_ch.drop(sig_ch_all.index)[['std2_vs_oop_wilcox']])
+condense_df(sig_ch.drop(sig_ch_all.index)[['std1_vs_oop_wilcox']])
 #%% plot confidence bands
 #plot_channels = ['13HEAD0000HFACYA',
 #                 '13CHST0000HFACYC',
@@ -35,10 +36,14 @@ for ch in plot_channels:
     x1 = arrange_by_group(dataset.table.query('HF_POS<3'), dataset.timeseries[ch], 'HF_POS')
     x2 = arrange_by_group(dataset.table.query('HF_POS==3'), dataset.timeseries[ch], 'MODEL')
     ax, lines = plot_bands(ax, dataset.t, x1, legend=False)
-    ax = plot_overlay(ax, dataset.t, x2, line_specs={k: {'linewidth': 0.5, 'color': 'b'} for k in x2.keys()})
+    ax = plot_overlay(ax, dataset.t, x2, line_specs={k: {'linewidth': 0.5} for k in x2.keys()})
+#    ax = plot_overlay(ax, dataset.t, x2, line_specs={k: {'linewidth': 0.5, 'color': 'b'} for k in x2.keys()})
     lines.extend(ax.lines[-len(x2):])
     ax = set_labels(ax, {'title': ch})
     ax.legend(lines, ['STD POS 1','STD POS 2'] + list(x2.keys()), bbox_to_anchor=(1,1))
+    fig.savefig(dataset.directory + 'TS_' + ch + '.png', bbox_inches='tight')
+    plt.show()
+    plt.close(fig)
 
 #%% plot peak values
 plot_channels = ['Max_13HEAD0000HFACYA',
@@ -53,11 +58,27 @@ plot_channels = ['Max_13HEAD0000HFACYA',
                  'Min_13NECKUP00HFFOXA',
                  'Min_13FEMRLE00HFFOZB',
                  'Min_13FEMRRI00HFFOZB',
-                 'Min_13ILACLE00HFFOXA']
+                 'Min_13ILACLE00HFFOXA',
+                 'Max_13NIJCIPTEHF00YX',
+                 'Max_13NIJCIPTFHF00YX',
+                 'Min_13RIBS01LEHFDSXB',
+                 'Min_13RIBS01RIHFDSXB',
+                 'Min_13RIBS02LEHFDSXB',
+                 'Min_13RIBS02RIHFDSXB',
+                 'Min_13RIBS03LEHFDSXB',
+                 'Min_13RIBS03RIHFDSXB',
+                 'Min_13RIBS04LEHFDSXB',
+                 'Min_13RIBS04RIHFDSXB',
+                 'Min_13RIBS05LEHFDSXB', 
+                 'Min_13RIBS05RIHFDSXB',
+                 'Min_13RIBS06LEHFDSXB',
+                 'Min_13RIBS06RIHFDSXB']
 for ch in plot_channels:
     fig, ax = plt.subplots()
     ax = sns.barplot(x='HF_POS', y=ch, data=pd.concat((dataset.features[ch].abs(), dataset.table['HF_POS']), axis=1))
     ax = set_labels(ax, {'title': ch})
+    plt.show()
+    plt.close(fig)
 
 
 #%% plot peak values as a function of installation stuff
